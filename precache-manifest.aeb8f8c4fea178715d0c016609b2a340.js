71b78bef9c5cb8d8b49077ba7f614f48da91b18c
silnet <995473428@qq.com>
2019-10-17 17:44:17 +0800

    Updates

---
+++ precache-manifest.aeb8f8c4fea178715d0c016609b2a340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ad9a2b700d208b42eba0b2c1b1d657",
+    "url": "git@github.com:qq995473428/jianshupage.git/index.html"
+  },
+  {
+    "revision": "81cc4239149a15050352",
+    "url": "git@github.com:qq995473428/jianshupage.git/static/js/2.18efe5d4.chunk.js"
+  },
+  {
+    "revision": "c6662e79cb460b44c366",
+    "url": "git@github.com:qq995473428/jianshupage.git/static/js/3.455a11a7.chunk.js"
+  },
+  {
+    "revision": "9c3d78ad7d36d46c9e11",
+    "url": "git@github.com:qq995473428/jianshupage.git/static/js/main.f333fbb0.chunk.js"
+  },
+  {
+    "revision": "c234143d6b568754b7f9",
+    "url": "git@github.com:qq995473428/jianshupage.git/static/js/runtime-main.70756290.js"
+  },
+  {
+    "revision": "2368c4b89cf48f063c4956747864b283",
+    "url": "git@github.com:qq995473428/jianshupage.git/static/media/3525704-218988490c02f8ad.2368c4b8.webp"
+  }
+]);
